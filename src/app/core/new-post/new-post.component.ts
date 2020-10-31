cfbedfb8f1f1e8de3daa817dcfe33d8617c52716
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-10-31 18:29:35 +0100

    new-post-v1

--- src/app/core/new-post/new-post.component.ts
+++ src/app/core/new-post/new-post.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { EventEmitter } from 'protractor';
+import { NewPostModel } from 'src/app/shared/models/new-post.model';
+import { SignIn } from 'src/app/shared/models/sign-in.models';
+import { StorageService } from 'src/app/shared/services/storage.service';
 
 @Component({
   selector: 'app-new-post',
@@ -11,7 +13,7 @@ export class NewPostComponent implements OnInit {
 
   formg: FormGroup
   date: Date = new Date()
-  constructor(private fb: FormBuilder) { this.createForm() }
+  constructor(private fb: FormBuilder,  private storage:StorageService) { this.createForm() }
 
   ngOnInit(): void {
   }
@@ -40,7 +42,7 @@ export class NewPostComponent implements OnInit {
 
   }
 
-  removeTags(i: number ) {
+  removeTags(i: number) {
 
     if (this.getTags.controls.length > 1) {
       this.getTags.removeAt(i)
@@ -51,7 +53,24 @@ export class NewPostComponent implements OnInit {
 
 
   saveForm() {
-    console.log(this.formg.controls);
+
+    if (this.formg.status === 'VALID') {
+      let publication: NewPostModel = new NewPostModel();
+
+      let user:SignIn = this.storage.getUser();
+      publication.title = this.formg.get('title').value;
+      publication.date = this.formg.get('date').value;
+      publication.tags = this.formg.get('tags').value;
+      publication.subtitle = this.formg.get('subtitle').value;
+      publication.urlImage = this.formg.get('urlImage').value;
+      publication.textImage = this.formg.get('textImage').value;
+      publication.description = this.formg.get('description').value;
+      publication.author = `${user.name}, ${user.lastname}, ${user.job}, ${user.age}, `;
+      publication.authorImage = user.urlImage;
+
+    } else {
+      alert('Rellene todos los campos por favor');
+    }
 
   }
 
---
+++ src/app/shared/models/new-post.model.ts
@@ -0,0 +1,13 @@
+export class NewPostModel {
+
+    title:string;
+    date:string;
+    tags:string[];
+    subtitle:string;
+    urlImage:string;
+    textImage:string;
+    description:string;
+    author:string; //nombre, apellido, trabajo, edad
+    authorImage:string;
+    constructor() {}
+}
