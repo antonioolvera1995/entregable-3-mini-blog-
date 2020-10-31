ad6d9575b15e14ab9e0d15e671c79273457ea768
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-10-31 18:10:19 +0100

    new-post-v0

--- src/app/core/new-post/new-post.component.ts
+++ src/app/core/new-post/new-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-new-post',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPostComponent implements OnInit {
 
-  constructor() { }
+  formg: FormGroup
+  date: Date = new Date()
+  constructor(private fb: FormBuilder) { this.createForm() }
 
   ngOnInit(): void {
   }
+
+
+  createForm() {
+    this.formg = this.fb.group({
+
+      title: ['', [Validators.required]],
+      date: [`${this.date}`, [Validators.required]],
+      tags: this.fb.array(['']),
+      subtitle: ['', [Validators.required]],
+      urlImage: ['', [Validators.required]],
+      textImage: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+
+    });
+  }
+  get getTags(): FormArray { return this.formg.get('tags') as FormArray; }
+  addTags(e) {
+
+
+    if (e.key.toLowerCase() === 'enter' && this.getTags.controls.length < 5 && this.getTags.status === 'VALID') {
+      this.getTags.push(this.fb.control('', [Validators.required]))
+    }
+
+  }
+
+  removeTags(i: number ) {
+
+    if (this.getTags.controls.length > 1) {
+      this.getTags.removeAt(i)
+    }
+
+  }
+
+
+
+  saveForm() {
+    console.log(this.formg.controls);
+
+  }
+
 
 }
