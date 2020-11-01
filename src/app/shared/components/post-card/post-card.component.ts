07006485befbb877dfc3f5cb426ad7ce635cd0ba
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-11-01 13:19:35 +0100

    publications-v0

--- src/app/shared/components/post-card/post-card.component.ts
+++ src/app/shared/components/post-card/post-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-card.component.css']
 })
 export class PostCardComponent implements OnInit {
+
+  @Input() urlImage:string = '';
+  @Input() title:string = '';
+  @Input() description:string = '';
+  @Input() date:string = '';
 
   constructor() { }
 
