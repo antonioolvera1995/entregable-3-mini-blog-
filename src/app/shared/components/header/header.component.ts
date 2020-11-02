7f29015be6f64ffd8745c0a7bf84efc100e5fe42
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-11-02 18:39:22 +0100

    styles-v2

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -13,6 +13,9 @@ export class HeaderComponent implements OnInit {
   userLoged:SignIn;
   public login: boolean = false;
   detalles:boolean = false;
+  width:number = 0;
+
+  mobile:boolean = false;
   
 
   constructor(private loginService: LoginService, private storage:StorageService) { }
@@ -27,6 +30,16 @@ export class HeaderComponent implements OnInit {
 
   start = setInterval(() => {
 
+    //reponsive
+    this.width = window.innerWidth
+    if (this.width <= 992) {
+      this.mobile = true;
+    }else{
+      this.mobile = false;
+    }
+
+
+    //reveal-text
     if (window.location.pathname.search('publication-details')>-1) {
       this.detalles = true;
     } else {
